72425f0b1f87ee48854b4f4262642feba41bb593
Rui Afonso Pereira <rui@deltaworx.eu>
2016-07-05 16:58:11 +0100

    Add form element to page

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ import RadioButton from './RadioButton';
 const App = () =>
   <div>
     <div className="col-md-8">
-      <h1>Register:</h1>
-      <FormGroup label="Name">
-        <Input
-          type="text"
-          className="form-control"
-          placeholder="Name"
-        />
-      </FormGroup>
-      <FormGroup label="Password">
-        <Input
-          type="password"
-          className="form-control"
-          placeholder="Password"
-        />
-      </FormGroup>
-      <FormGroup label="Gender">
-        <RadioButton labels={['female', 'male']} />
-      </FormGroup>
+      <form>
+        <h1>Register:</h1>
+        <FormGroup label="Name">
+          <Input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+          />
+        </FormGroup>
+        <FormGroup label="Password">
+          <Input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </FormGroup>
+        <FormGroup label="Gender">
+          <RadioButton
+            labels={['female', 'male', 'other']}
+            name="gender"
+          />
+        </FormGroup>
+      </form>
     </div>
     <div className="col-md-4">TODO</div>
   </div>;
