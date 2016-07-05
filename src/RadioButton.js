362a9be25533417c4fabf451b55e8c0690b985e4
Rui Afonso Pereira <rui@deltaworx.eu>
2016-07-05 16:58:52 +0100

    Fix bug preventing to correctly select radio buttons

--- src/RadioButton.js
+++ src/RadioButton.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 const RadioButton = (props) => (
   <div className="radio">
-    <label className="radio-inline">
-      {props.labels.map((label, index) => {
-        return (
-          <div key={index}>
-            <input
-              type="radio"
-              name="optradio"
-            /> {label}
-          </div>
-        );
-      })}
-    </label>
+    {props.labels.map((label, index) => {
+      return (
+        <label className="radio-inline" key={index}>
+          <input
+            type="radio"
+            name={props.name}
+          /> {label}
+        </label>
+      );
+    })}
   </div>
 );
 
 RadioButton.propTypes = {
-  labels: React.PropTypes.array.isRequired
+  labels: React.PropTypes.array.isRequired,
+  name: React.PropTypes.string.isRequired
 };
 
 export default RadioButton;
