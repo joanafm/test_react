4c591302dead6565fcc867f83611041af8aa6426
Joana Martins <joana_fmartins@outlook.com>
2016-07-07 23:57:28 +0100

    Update CSS

--- src/FormGroup.js
+++ src/FormGroup.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const FormGroup = ({ label, children }) =>
   <div className="form-group">
-    <label>{label}</label>
+    <label>
+      <span className="glyphicon glyphicon-asterisk" aria-hidden="true"></span>
+      {label}
+    </label>
     {children}
   </div>;
 
