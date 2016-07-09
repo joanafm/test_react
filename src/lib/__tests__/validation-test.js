b1b379893d54f6f8a66e0a9c33d16882d263a8e4
Joana Martins <joana_fmartins@outlook.com>
2016-07-09 10:09:05 +0100

    Add tests to isValidPassword

--- src/lib/__tests__/validation-test.js
+++ src/lib/__tests__/validation-test.js
@@ -6,8 +6,33 @@ describe('validation rules', () => {
   /**
    * isValidPassword
    */
-   // ...
-   // ...
+  describe('isValidPassword', () => {
+    it('should return false if an empty string is provided', () => {
+      const string = '';
+      expect(validations.isValidPassword(string)).toBeFalsy();
+    });
+
+    it('should return false if the strings length is less than six', () => {
+      const string = 'Joana';
+      expect(validations.isValidPassword(string)).toBeFalsy();
+    });
+
+    it('should return false if the string only contains letters', () => {
+      const string = 'JoanaM';
+      expect(validations.isValidPassword(string)).toBeFalsy();
+    });
+
+    it('should return false if the string only contains numbers', () => {
+      const string = '123456';
+      expect(validations.isValidPassword(string)).toBeFalsy();
+    });
+
+    it('should return true if the string contains letters and numbers, and the ' +
+      'length is >= six', () => {
+      const string = 'Joana5';
+      expect(validations.isValidPassword(string)).toBeTruthy();
+    });
+  });
 
   /**
    * isEmpty
