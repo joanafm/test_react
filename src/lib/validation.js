8e1a0f9875346ff577f6638e42b0efad1794adb1
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-07-09 08:56:13 +0100

    Code cleanup

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,33 +1,26 @@
-export function isValidPassword(password) {
-  const pattLetterNumber = new RegExp('[^a-zA-Z0-9]');
-  const pattLetter = new RegExp('[a-zA-Z]');
-  const pattNumber = new RegExp('[0-9]');
+const pattLetterNumber = new RegExp('[^a-zA-Z0-9]');
+const pattLetter = new RegExp('[a-zA-Z]');
+const pattNumber = new RegExp('[0-9]');
 
-  if (password === '') {
+export function isValidPassword(input) {
+  if (input === '') {
     return false;
-  } else if (password !== '') {
-    if (
-      pattLetterNumber.test(password) ||
-      password.length < 6 ||
-      !pattLetter.test(password) ||
-      !pattNumber.test(password)
-    ) {
-      return false;
-    }
   }
+
+  if (pattLetterNumber.test(input) ||
+      input.length < 6 ||
+      !pattLetter.test(input) ||
+      !pattNumber.test(input)) {
+    return false;
+  }
+
   return true;
 }
 
 export function isValidName(name) {
-  if (name === '') {
-    return false;
-  }
-  return true;
+  return name !== '';
 }
 
 export function isValidGender(gender) {
-  if (gender === '') {
-    return false;
-  }
-  return true;
+  return gender !== '';
 }
